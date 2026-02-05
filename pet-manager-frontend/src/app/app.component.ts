0667ef465c2ab4fd3633612a4f220ee6636ef4d8
fmccolantonio <fmccolantonio@gmail.com>
2026-02-05 19:49:57 +0000

    Final: Correção da Navbar no Login e entrega final do projeto

--- pet-manager-frontend/src/app/app.component.ts
+++ pet-manager-frontend/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class AppComponent {
   title = 'pet-manager-frontend';
+  showSidebar = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showSidebar = !event.urlAfterRedirects.includes('/login');
+    });
+  }
 }
